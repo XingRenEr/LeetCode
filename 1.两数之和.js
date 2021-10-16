bf8c886b08e3a3006d016da0f854761aa6aa6e6a
XingRenEr <joanna19970110@foxmail.com>
2021-10-16 15:59:53 +0800

    修改 - 2.两数相加

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -10,8 +10,18 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-
+var twoSum = function (nums, target) {
+    let hash = {};
+    let length = nums.length;
+    
+    for (let i = 0; i < length; i++) {
+        const num = nums[i];
+        if (hash[target - num] != undefined) return [hash[target - num], i];
+        hash[num] = i;
+    }
 };
 // @lc code=end
 
+nums = [2,7,11,15], target = 9;
+res = twoSum(nums, target);
+console.log(res);
